7ce39f2578d1218d51ebdc947d5274c08fff6a9d
mocheer <mocheer@foxmail.com>
2026-07-19 15:28:29 +0800

    update

--- pkg/ts/img/resize.go
+++ pkg/ts/img/resize.go
@@ -13,5 +13,5 @@ func (p *Img) Resize(width, height int) *Img {
 
 // Resize 当宽度或者高度为0时，保持比例
 func Resize(target image.Image, width, height int) image.Image {
-	return imaging.Resize(target, width, height, imaging.Lanczos)
+	return imaging.Resize(target, width, height, imaging.Linear)
 }
